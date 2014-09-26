cfe84701f8b99a27e14e18fc6f15db45108229f4
Speich <simon.speich@gmail.com>
2014-09-26 13:29:27 +0200

    implement insert after dropping (work in progress)

--- js/app.js
+++ js/app.js
@@ -29,7 +29,6 @@ define([
 			for (i = 0, len = nl.length; i < len; i++) {
 				nl[i].setAttribute('draggable', true);
 
-
 				on(nl[i], 'dragstart', function(evt) {
 					var i, len, node, nl;
 
@@ -55,17 +54,20 @@ define([
 				});
 			}
 
-			on(d.getElementsByTagName('main')[0], '.overlayContainer:drop', lang.hitch(this, this.drop));
+			on(d.getElementsByTagName('main')[0], '.overlayContainer:drop', this.drop);
 		},
 
 		drop: function(evt) {
-			var cpSource = this.parentNode,
-				overlay = evt.target,
-				cl = overlay.classList,
-				overlayContainer = query(overlay).parents('.overlayContainer')[0],
-				targetContainer = query(overlayContainer).parents('.contentPane, .paneDivider')[0],
+			var overlay = evt.target,
+				overlayContainer = overlay.parentNode,
+				overlays = overlayContainer.children,
+				targetContainer = overlayContainer.parentNode,
 				isDivider = targetContainer.classList.contains('paneDivider'),
-				flexDir, tabOrder, tabNav;
+				flexDir, tabNav,
+				elIdx,
+				cl = overlay.classList;
+
+			console.log(this);
 
 			if (cl.contains('overlayMiddle')) {
 				// add to tabs
@@ -74,13 +76,19 @@ define([
 			}
 			else if (cl.contains('overlayEdge')) {
 				flexDir = d.defaultView.getComputedStyle(overlayContainer, '').getPropertyValue('flex-direction');
-				tabOrder = d.defaultView.getComputedStyle(overlay, '').getPropertyValue('order');
-				console.log(flexDir, overlay, tabOrder);
+				console.log(flexDir, overlay);
 				if (isDivider) {
 					// dropping on divider
+
+					//targetContainer.insertBefore()
+
 				}
 				else {
-					// add new container (where? top left bottom right?)
+					// find if we dropped on first or last child, e.g. top, left, bottom or right overlay to know where to insert new cp
+					elIdx = Array.prototype.indexOf.call(overlays, overlay);
+
+					console.log(elIdx, overlays.length);
+					// add new container (where did we drop? top left bottom right?)
 					// use css order property together with flex-direction to find which edge we dropped on
 					// -> convert contentPane to paneContainer and add old and new panes to it
 
