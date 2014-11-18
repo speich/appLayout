ec46ddc6b1337733abca00137e804aba126a6adb
Simon <simon.speich@gmail.com>
2014-11-18 13:14:11 +0100

    handle dropping (work in progress)

--- js/layout.js
+++ js/layout.js
@@ -2,13 +2,14 @@ define([
 	'dojo/_base/lang',
 	'dojo/on',
 	'dojo/query',
+	'appLayout/domUtil',
 	'appLayout/layout/Divider',
 	'appLayout/layout/overlayFactory',
 	'appLayout/layout/paneFactory',
 	'appLayout/layout/dividerFactory',
 	'appLayout/layout/tabBarFactory',
 	'dojo/NodeList-traverse'
-], function(lang, on, query, Divider, overlayFactory, paneFactory, dividerFactory, tabBarFactory) {
+], function(lang, on, query, domUtil, Divider, overlayFactory, paneFactory, dividerFactory, tabBarFactory) {
 	'use strict';
 
 	var d = document,
@@ -23,46 +24,78 @@ define([
 		},
 
 		initDnd: function() {
+			var self = this;
 			tabBarFactory.initDndAll();
 
-			on(d.getElementsByTagName('main')[0], '.overlayContainer:drop', lang.hitch(this, function(evt) {
-				this.drop(evt);
-			}));
+			on(d.getElementsByTagName('main')[0], '.overlayContainer:drop', function(evt) {
+				// TODO: handle special case of dropping last tab of a content pane, e.g. remove that pane and divider
+				lang.hitch(self, self.drop(this, evt.target));
+			});
 		},
 
-		drop: function(evt) {
-			var pane, divider, node,
-				overlay = evt.target,
-				cl = overlay.classList,
-				overlayContainer = query(overlay).parents('.overlayContainer')[0],
-				targetContainer = query(overlayContainer).parents('.contentPane, .paneDivider')[0],
+		drop: function(source, target) {
+console.log(source, target);
+
+			var pane, divider, nodeDivider, idx, type, targetParent, paneContainer,
+				sourceParent = source.parent,
+				targetOverlay = target,
+				cl = targetOverlay.classList,
+				targetOverlayContainer = query(targetOverlay).parents('.overlayContainer')[0],// note: overlay containers can have different dom (overlay over divider vs. overlay over contentPane)
+				targetContainer = query(targetOverlayContainer).parents('.contentPane, .paneDivider')[0],
 				isDivider = targetContainer.classList.contains('paneDivider'),
 				tab = tabBarFactory.getDndData();
 
-			if (cl.contains('middleOverlay')) {
-				// add new tab
+			/*
+			if (!this.allowDrop(source, target)) {
+				return false;
+			}
+*/
+			// dropping on a divider -> add a new contentPane
+			if (isDivider) {
+				// dropping on divider -> add a new contentPane
+				pane = paneFactory.insertBefore(targetContainer, tab.head, tab.cont);
+				// add a new divider
+				nodeDivider = dividerFactory.insertBefore(pane);
+				this.addDivider(nodeDivider);
+			}
+			// add a new tab
+			else if (cl.contains('middleOverlay')) {
 				tabBarFactory.addTab(targetContainer, tab.head, tab.cont);
 			}
-			else if (cl.contains('edgeOverlay')) {
-				if (isDivider) {
-					// dropping on divider
-					// add a new contentPane
-					pane = paneFactory.insertBefore(targetContainer, tab.head, tab.cont);
-					// add a new divider
-					node = dividerFactory.insertBefore(pane);
+			// split pane container
+			else {
+				// re-init the divider (=targetContainer) we dropped on, since it has new neighbor (contentPane).
+				this.resetDividers();
 
-					divider = new Divider();
-					divider.init(node);
-					// re-init the divider (=targetContainer) we dropped on, since it has new neighbor (contentPane).
-					this.resetDividers();
-					registryDividers.push(divider);
+				if (source === targetOverlayContainer) {
+					// TODO: does not work on created pane yet after already toggling parent
+					overlayFactory.toggleClass(source);
+				}
+				// TODO: remove width of old neigbors for Divider to work properly
+				// TODO: move to new method split()?
+				// add new container (where? top left bottom right?)
+				// use css order property together with flex-direction to find which edge we dropped on
+				// -> convert contentPane to paneContainer and add old and new panes to it
+				idx = domUtil.getElementIndex(targetOverlay);
+				targetParent = targetContainer.parentNode;
+				type = targetParent.classList.contains('rowContainer') ? 'col' : 'row';
+				paneContainer = paneFactory.createPaneContainer(type);
+				targetParent.insertBefore(paneContainer, targetContainer);
+				paneContainer.appendChild(targetContainer);
+				nodeDivider = dividerFactory.create(type);
+				pane = paneFactory.createContentPane(tab.head, tab.cont);
+				if (idx === 0) {
+					paneContainer.insertBefore(nodeDivider, targetContainer);
+					paneContainer.insertBefore(pane, nodeDivider);
 				}
 				else {
-					// add new container (where? top left bottom right?)
-					// use css order property together with flex-direction to find which edge we dropped on
-					// -> convert contentPane to paneContainer and add old and new panes to it
-
+					paneContainer.appendChild(nodeDivider);
+					paneContainer.appendChild(pane);
 				}
+
+				this.addDivider(nodeDivider);
+
+//this.reinitDividers();
 			}
 		},
 
@@ -80,13 +113,24 @@ define([
 			}
 		},
 
+		addDivider: function(node){
+			var divider = new Divider();
+			divider.init(node);
+			registryDividers.push(divider);
+		},
+
 		resetDividers: function() {
 			registryDividers.forEach(function(divider) {
 				divider.removeEvents();
+				divider.resetNodes();
 				divider.init(divider.domNode);
 			});
 		},
-
+reinitDividers: function() {
+	registryDividers.forEach(function (divider) {
+		divider.init(divider.domNode);
+	});
+},
 		initEvents: function() {
 			var i, len,
 				nl = d.getElementsByClassName('overlay');
